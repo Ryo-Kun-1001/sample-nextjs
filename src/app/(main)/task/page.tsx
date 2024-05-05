471ffbea81ba2db4863f8a381d4f8bb6cc11e933
Ryo-kun-1001 <r.maeda+github@pon3.dev>
2024-05-05 21:08:14 +0900

    ローディングコンポーネント

--- src/app/(main)/task/page.tsx
+++ src/app/(main)/task/page.tsx
@@ -3,7 +3,7 @@ import { Task } from "@/app/api/tasks/route";
 const getTasks = async () => {
   const response = await fetch("http://localhost:3000/api/tasks", {
     method: "GET", // 省略可
-    // cache: "no-store", // データ取得のキャッシュ機能をオフにできる データが頻繁に変更されるページに有効
+    cache: "no-store", // データ取得のキャッシュ機能オフ データが頻繁に変更されるページに有効
   });
   return await response.json();
 };
